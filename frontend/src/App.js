816a7706c319d7995de8da9ede9c349c862d3e7c
egordorosh <egordororsh@gmail.com>
2024-03-10 18:37:22 +0100

    build frontend project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,6 @@ import AdminPage from '@/pages/page.js';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-
       <AdminPage />
     </div>
   );
