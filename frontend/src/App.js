f44b9e6ca6996e025b5ce4cfbebfdec2b62bb6ed
egordorosh <egordororsh@gmail.com>
2024-03-10 18:30:41 +0100

    add react frontend-crm project

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import AdminPage from '@/pages/page.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+
+      <AdminPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/page.js
@@ -0,0 +1,38 @@
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
+import { useState, useEffect } from "react";
+import AdminEmployeesModule from "@/components/AdminEmployeesModule";
+import AdminServicesModule from "@/components/AdminServicesModule";
+import employeesService from "@/services/employees.service";
+
+export default function AdminPage() {
+  const [employees, setEmployees] = useState(null);
+
+  const fetchEmployees = async () => {
+    const data = await employeesService.getEmployees();
+    return setEmployees(data);
+  };
+
+  useEffect(() => {
+    fetchEmployees();
+  }, []);
+
+  return (
+    <main>
+      <Box sx={{ width: "100%", maxWidth: 768 }}>
+        <Typography variant="h2">ADMIN</Typography>
+
+        <AdminEmployeesModule 
+          employees={employees || []}
+          fetchEmployees={fetchEmployees}
+        />
+
+        <Divider />
+
+        <AdminServicesModule employees={employees || []} />
+
+      </Box>
+    </main>
+  );
+}
