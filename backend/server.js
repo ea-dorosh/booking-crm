aca618c7de2346b83a8d5b6fcfc37a135430cda5
egordorosh <egordororsh@gmail.com>
2024-04-08 20:24:38 +0200

    add comments

--- backend/server.js
+++ backend/server.js
@@ -12,27 +12,31 @@ app.use(morgan('dev'));
 
 const port = process.env.PORT || 3500;
 
-app.use(cors()); 
+app.use(cors());
 app.use(express.json());
 
+// routes for CRM
+const authRouter = require('./routes/auth');
 const servicesRouter = require('./routes/services/services');
 const adminRouter = require('./routes/admin/admin');
-const calendarRouter = require('./routes/calendar/calendar');
-const appointmentsRouter = require('./routes/appointments/appointments');
 const employeesRouter = require('./routes/employees/employees');
 
-// Import authentication routes
-const authRouter = require('./routes/auth');
+// routes for client site
+const calendarRouter = require('./routes/calendar/calendar');
+const appointmentsRouter = require('./routes/appointments/appointments');
 
 // Use authentication routes
 app.use('/auth', authRouter);
 app.use('/api', databaseMiddleware);
 
+// Use CRM routes
 app.use('/api/services', servicesRouter);
 app.use('/api/admin', adminRouter);
+app.use('/api/employees', employeesRouter);
+
+// Use client site routes
 app.use('/api/calendar', calendarRouter);
 app.use('/api/appointments', appointmentsRouter);
-app.use('/api/employees', employeesRouter);
 
 app.listen(port, '192.168.178.27', () => {
   console.log(`Server is running on port ${port}`);
