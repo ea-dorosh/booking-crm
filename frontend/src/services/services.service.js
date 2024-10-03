cecf96fde2f009efed68cdd72f3b9c2dc93f5a2a
egordorosh <egordororsh@gmail.com>
2024-10-03 12:14:26 +0200

    FE: use generic method 'appendFormData' in updateServiceCategory service

--- frontend/src/services/services.service.js
+++ frontend/src/services/services.service.js
@@ -1,29 +1,18 @@
 import axios from 'axios';
 import ERRORS from '@/constants/errors';
 
-// function appendFormData(data, formData = new FormData(), parentKey = '') {
+function appendFormData(data, formData = new FormData(), parentKey = '') {
+  if (data && typeof data === 'object' && !(data instanceof File)) {
+    Object.keys(data).forEach(key => {
+      const fullKey = parentKey ? `${parentKey}[${key}]` : key;
 
-//   console.log(`appendFormData called with parentKey: ${parentKey}, data:`, data);
-
-//   if (data && typeof data === 'object' && !(data instanceof File)) {
-//     console.log(`appendFormData iterating over keys of:`, data);
-//     Object.keys(data).forEach(key => {
-//       const fullKey = parentKey ? `${parentKey}[${key}]` : key;
-//       console.log(`Recursing into key: ${key}, fullKey: ${fullKey}`);
-//       appendFormData(data[key], formData, fullKey);
-//     });
-//   } else {
-//     // If the data is a primitive type or a File, append it to FormData
-//     console.log(`Appending to FormData: key = ${parentKey}, value =`, data);
-//     formData.append(parentKey, data);
-//   }
-//   return formData;
-// }
-
-// function objectToFormData(obj) {
-//   console.log('Starting objectToFormData with object:', obj);
-//   return appendFormData(obj);
-// }
+      appendFormData(data[key], formData, fullKey);
+    });
+  } else {
+    formData.append(parentKey, data);
+  }
+  return formData;
+}
 
 const token = localStorage.getItem(`token`);
 
@@ -108,20 +97,7 @@ const updateService = async (service) => {
 };
 
 const updateServiceCategory = async (serviceCategory) => {  
-
-  // const formData = objectToFormData(serviceCategory);
-
-  const formData = new FormData();
-
-  // Append the id and name to the FormData object
-  formData.append('id', serviceCategory.id);
-  formData.append('name', serviceCategory.name);
-
-
-  // Append the image file if it exists
-  if (serviceCategory.image) {
-    formData.append('image', serviceCategory.image);
-  }
+  const formData = appendFormData(serviceCategory);
 
   try {    
     const response = await axios.put(
@@ -136,35 +112,11 @@ const updateServiceCategory = async (serviceCategory) => {
 
     return response.data;
   } catch (error) {
-    // Handle the error (axios wraps errors into its own object)
     if (error.response && error.response.status === ERRORS.VALIDATION_ERROR) {
       throw new Error(JSON.stringify(error.response.data.errors));
     }
     throw error;
   }
-  
-  // eslint-disable-next-line no-useless-catch
-  // try {
-  //   const response = await fetch(`${process.env.REACT_APP_API_URL}api/protected/services/category/edit/${serviceCategory.id}`, {
-  //     method: "PUT",
-  //     headers: {
-  //       Authorization: `Bearer ${token}`,
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({ category: serviceCategory }),
-  //   });
-
-  //   if(response.status === ERRORS.VALIDATION_ERROR) {
-  //     const data = await response.json();
-  //     throw new Error(JSON.stringify(data.errors));
-  //   }
-
-  //   const data = await response.json();
-    
-  //   return data;
-  // } catch (error) {
-  //   throw error;
-  // }
 };
 
 const deleteService = async (id) => {
