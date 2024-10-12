d01c5afb6a66dcc59d984b3bb5058125e57d29e3
egordorosh <egordororsh@gmail.com>
2024-10-12 18:17:00 +0200

    BE add check if customer already exists

--- backend/routes/appointments/appointments.js
+++ backend/routes/appointments/appointments.js
@@ -4,6 +4,10 @@ const { getServiceDuration } = require('../../utils/timeUtils');
 const { formattedName, formattedPhone } = require('../../utils/formatters');
 const { validateCustomerForm } = require('./appointmentsUtils');
 const { getAppointmentEndTime } = require('../calendar/calendarUtils');
+const dayjs = require('dayjs');
+const advancedFormat = require('dayjs/plugin/advancedFormat');
+
+dayjs.extend(advancedFormat);
 
 const checkEmployeeTimeNotOverlap = async ({db, res}, {date, employeeId, timeStart, timeEnd }) => {
   const checkAvailabilityQuery = `
@@ -17,7 +21,16 @@ const checkEmployeeTimeNotOverlap = async ({db, res}, {date, employeeId, timeSta
     )
   `;
 
-  const checkAvailabilityValues = [employeeId, date, timeStart, timeEnd, timeStart, timeEnd, timeStart, timeEnd];
+  const checkAvailabilityValues = [
+    employeeId, 
+    date, 
+    timeStart, 
+    timeEnd, 
+    timeStart,
+    timeEnd, 
+    timeStart, 
+    timeEnd,
+  ];
 
   try {
     const [existingAppointments] = await db.query(checkAvailabilityQuery, checkAvailabilityValues);
@@ -39,7 +52,6 @@ router.post(`/create`, async (req, res) => {
   if (Object.keys(errors).length > 0) {
     return res.status(428).json({ errors });
   }
-console.log(`appointmentAndCustomer`, appointmentAndCustomer);
 
   let serviceDurationAndBufferTimeInMinutes;
   try {
@@ -50,30 +62,48 @@ console.log(`appointmentAndCustomer`, appointmentAndCustomer);
     //get service duration (service_time + buffer_time)
     serviceDurationAndBufferTimeInMinutes = getServiceDuration(serviceRows[0].duration_time, serviceRows[0].buffer_time);
   } catch (error) {
-    console.log(`1 catch (error)`, error);
     return res.status(500).json(error);
   }
 
-  const customerQuery = `
-      INSERT INTO Customers (first_name, last_name, email, phone)
-      VALUES (?, ?, ?, ?)
-    `;
+  // check customer already exists
+  const customerCheckQuery = `
+    SELECT customer_id FROM Customers
+    WHERE email = ?
+  `;
 
-  const customerValues = [
-    formattedName(appointmentAndCustomer.firstName),
-    formattedName(appointmentAndCustomer.lastName),
-    appointmentAndCustomer.email,
-    formattedPhone(appointmentAndCustomer.phone),
-  ];
-
+  let isCustomerNew = 1; // 0 - Existing Customer, 1 - New Customer
   let customerId;
 
   try {
-    const [customerResults] = await req.dbPool.query(customerQuery, customerValues);
-    customerId = customerResults.insertId;
+    const [customerResults] = await req.dbPool.query(customerCheckQuery, [appointmentAndCustomer.email]);
+
+    if (customerResults.length >= 1) {
+      isCustomerNew = 0; // 0 - Existing Customer
+      customerId = customerResults[0].customer_id;
+    }
   } catch (error) {
-    console.log(`2 catch (error)`, error);
     return res.status(500).json(error);
+  }
+
+  if (isCustomerNew === 1) { // New Customer
+    const customerQuery = `
+        INSERT INTO Customers (first_name, last_name, email, phone)
+        VALUES (?, ?, ?, ?)
+      `;
+
+    const customerValues = [
+      formattedName(appointmentAndCustomer.firstName),
+      formattedName(appointmentAndCustomer.lastName),
+      appointmentAndCustomer.email,
+      formattedPhone(appointmentAndCustomer.phone),
+    ];
+
+    try {
+      const [customerResults] = await req.dbPool.query(customerQuery, customerValues);
+      customerId = customerResults.insertId;
+    } catch (error) {
+      return res.status(500).json(error);
+    }
   }
 
   const employeeId = appointmentAndCustomer.employeeId;
@@ -88,9 +118,14 @@ console.log(`appointmentAndCustomer`, appointmentAndCustomer);
   }, {date, employeeId, timeStart, timeEnd });
 
   const appointmentQuery = `
-    INSERT INTO SavedAppointments (date, time_start, time_end, service_id, customer_id, service_duration, employee_id)
-    VALUES (?, ?, ?, ?, ?, ?, ?)
+    INSERT INTO SavedAppointments (date, time_start, time_end, service_id, customer_id, service_duration, employee_id, created_date, customer_first_name, customer_last_name, customer_email, customer_phone, is_customer_new)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
   `;
+
+  /** is_customer_new ENUM 
+   * 0 - Existing Customer
+   * 1 - New Customer
+  */
 
   const appointmentValues = [
     date,
@@ -100,6 +135,12 @@ console.log(`appointmentAndCustomer`, appointmentAndCustomer);
     customerId,
     serviceDurationAndBufferTimeInMinutes,
     employeeId,
+    dayjs().format('DD.MM.YYYY HH:mm:ss'),
+    formattedName(appointmentAndCustomer.firstName),
+    formattedName(appointmentAndCustomer.lastName),
+    appointmentAndCustomer.email,
+    formattedPhone(appointmentAndCustomer.phone),
+    isCustomerNew,
   ];
 
 
