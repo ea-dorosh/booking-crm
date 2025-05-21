af3c8246ca43fa08c8f2f7171a4eab8b6de4d0fa
egordorosh <egordororsh@gmail.com>
2025-05-21 19:34:32 +0200

    [FE] Change literals

--- frontend/src/utils/formData.js
+++ frontend/src/utils/formData.js
@@ -1,5 +1,5 @@
 export const appendFormData = (data, formData = new FormData(), parentKey = '') => {
-  if (data && typeof data === 'object' && !(data instanceof File)) {
+  if (data && typeof data === `object` && !(data instanceof File)) {
     Object.keys(data).forEach(key => {
       const fullKey = parentKey ? `${parentKey}[${key}]` : key;
 
